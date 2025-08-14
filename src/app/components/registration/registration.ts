f71a4fe6d2b668899a26d00d37f26309762291cc
gwelle <guillaume.welle.sio@gmail.com>
2025-08-14 22:16:00 +0200

    creation environments file, usersService (/POST)

--- src/app/components/registration/registration.ts
+++ src/app/components/registration/registration.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './registration.html',
   styleUrl: './registration.css'
 })
-export class Registration {
+export class Registration implements OnInit {
+  // Define any properties or methods needed for registration
 
+  /**
+   * constructor
+   * @param userService 
+   */
+  constructor(private readonly userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    // Perform any initialization logic here
+        console.log('Resgistration::ngOnInit()');
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  // Define any properties or methods needed for user management
+  private readonly apiUrl: string;
+
+  /**
+   * Constructor to inject HttpClient.
+   * @param http - HttpClient for making HTTP requests.
+   */
+  constructor(private readonly http: HttpClient) {
+    // Initialize the API URL from environment variables
+    this.apiUrl = environment.API_URL;
+  }
+
+  /**
+   *  Registers a new user.
+   * @param user 
+   * * This method registers a new user by sending a POST request to the API.
+   * It expects a User object as input and returns an Observable of the User type.
+   * @returns 
+   */
+  registrer(user:User) {
+    return this.http.post<User>(`${this.apiUrl}`, user);
+  }
+}
